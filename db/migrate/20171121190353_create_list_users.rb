841ef18a35b9333902d909ad2d27e3ef241718ae
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-21 15:30:35 -0600

    adds fields to list_users create migration, 'rich' join on list_items and users, so corresponding id fields and an index. migration has run

--- db/migrate/20171121190353_create_list_users.rb
+++ db/migrate/20171121190353_create_list_users.rb
@@ -1,8 +1,13 @@
 class CreateListUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :list_users do |t|
-
+      t.integer "list_item_id"
+      t.integer "user_id"
+      t.string "permissions"
       t.timestamps
     end
+
+    add_index("list_users", ["list_item_id", "user_id"])
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121051643) do
+ActiveRecord::Schema.define(version: 20171121190353) do
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "list_item_id"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20171121051643) do
   create_table "list_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "list_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "list_item_id"
+    t.integer "user_id"
+    t.string "permissions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_item_id", "user_id"], name: "index_list_users_on_list_item_id_and_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
