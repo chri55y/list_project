1bf288d98367978bc03849eecc1885cdfd28df95
chri55y <11790059+chri55y@users.noreply.github.com>
2017-11-20 23:23:37 -0600

    adds 2 fields to create_attachments migration

--- db/migrate/20171121051643_create_attachments.rb
+++ db/migrate/20171121051643_create_attachments.rb
@@ -1,7 +1,8 @@
 class CreateAttachments < ActiveRecord::Migration[5.1]
   def change
     create_table :attachments do |t|
-
+      t.integer "list_item_id"
+      t.integer "linked_account_id"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121050821) do
+ActiveRecord::Schema.define(version: 20171121051643) do
+
+  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "list_item_id"
+    t.integer "linked_account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "linked_accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
