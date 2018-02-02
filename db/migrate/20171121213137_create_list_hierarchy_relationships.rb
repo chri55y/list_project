eac83d66ae55dc8a096c005113d5627de4db34e1
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-01 19:06:12 -0600

    LHR par/chi: implementation 1.0 | adds plain integer columns, but FK adds not working (so commented out)

--- db/migrate/20171121213137_create_list_hierarchy_relationships.rb
+++ db/migrate/20171121213137_create_list_hierarchy_relationships.rb
@@ -1,8 +1,20 @@
 class CreateListHierarchyRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :list_hierarchy_relationships do |t|
-      t.integer "parent_item_id"
-      t.integer "child_item_id"
+      # t.integer "parent_item_id"
+      # t.integer "child_item_id"
+      #
+      # t.references :parent, references: :list_items
+      # t.references :child, references: :list_items
+
+      # t.references  :list_items, :parent
+      # t.references  :list_items, :child
+      #
+      # t.references  (:list_items, :parent)
+      # t.references  (:list_items, :child)
+
+      t.integer "parent_id", "child_id"
+
 
       t.boolean "active"
       t.integer "status"
@@ -17,5 +29,9 @@ class CreateListHierarchyRelationships < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
+
+    # add_foreign_key :list_hierarchy_relationships, :list_items, column: parent_id, primary_key: :id
+    # add_foreign_key :list_hierarchy_relationships, :list_items, column: child_id, primary_key: :id
+
   end
 end
