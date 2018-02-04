ed73e6e2b35124f320c18735b51b111cbbf001ee
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 23:59:08 -0600

    switch to resourceful routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/delete'
+  resources :users do
+    member do
+      get :delete
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
