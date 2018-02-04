b88c633fe8dc447e68025f5070bece7a3aca130f
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 21:51:03 -0600

    inverted order of creation to avoid validation error (linked accounts requires user)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-LinkedAccount.create!([
-  {user_id: 1}
-])
+
 User.create!([
   {username: "MarthaAnne", first_name: "Martha", last_name: "Bogdon", email: "", password: nil}
 ])
+LinkedAccount.create!([
+                          {user_id: 1}
+                      ])
