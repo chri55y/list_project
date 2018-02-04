c49935a5950f41474ea5941484797b261a4232a5
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 23:41:36 -0600

    attempt at defining LHR par/chi associations -> record can save, but cannot access .parents or .children

--- app/models/list_hierarchy_relationship.rb
+++ app/models/list_hierarchy_relationship.rb
@@ -1,2 +1,12 @@
 class ListHierarchyRelationship < ApplicationRecord
+  # associations
+  belongs_to :parent , foreign_key: "parent_id", class_name: "ListItem"
+  belongs_to :child , foreign_key: "child_id", class_name: "ListItem"
+
+  # scopes
+
+
+  # validations
+
+
 end
