8a811edf93af9fa11e05fe45c8fbaf1b981f015d
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 22:53:41 -0600

    clean up dumped seed file (added list_items for testing LHR)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,19 @@ User.create!([
                  {username: "MarthaAnne", first_name: "Martha", last_name: "Bogdon", email: "", password: nil},
                  {username: "WadyBug", first_name: "Wade", last_name: "Bogdon", email: "", password: nil}
              ])
+
 LinkedAccount.create!([
   {user_id: 1}
 ])
 ListItem.create!([
-  {name: "feed okra"}
+  {name: "feed okra"},
+  {name: "open bag"},
+  {name: "measure food with scoop"},
+  {name: "pour food in bowl"},
+  {name: "return scoop"},
+  {name: "close bag"}
 ])
 ListUser.create!([
   {list_item_id: 1, user_id: 2, permissions: nil},
   {list_item_id: 1, user_id: 1, permissions: nil}
 ])
-
