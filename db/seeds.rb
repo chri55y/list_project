77aa054fa9b8b50df649f596cdd88f6a58a1be9e
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 22:13:11 -0600

    re-order dumped seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+User.create!([
+                 {username: "MarthaAnne", first_name: "Martha", last_name: "Bogdon", email: "", password: nil},
+                 {username: "WadyBug", first_name: "Wade", last_name: "Bogdon", email: "", password: nil}
+             ])
 LinkedAccount.create!([
   {user_id: 1}
 ])
@@ -8,7 +12,4 @@ ListUser.create!([
   {list_item_id: 1, user_id: 2, permissions: nil},
   {list_item_id: 1, user_id: 1, permissions: nil}
 ])
-User.create!([
-  {username: "MarthaAnne", first_name: "Martha", last_name: "Bogdon", email: "", password: nil},
-  {username: "WadyBug", first_name: "Wade", last_name: "Bogdon", email: "", password: nil}
-])
+
