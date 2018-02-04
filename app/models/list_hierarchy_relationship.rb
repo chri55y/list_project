0e7e56a67e81f48947e5d625464455e1afa4c15a
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-03 23:42:29 -0600

    commenting out LHR par/chi logic, for now

--- app/models/list_hierarchy_relationship.rb
+++ app/models/list_hierarchy_relationship.rb
@@ -1,7 +1,7 @@
 class ListHierarchyRelationship < ApplicationRecord
   # associations
-  belongs_to :parent , foreign_key: "parent_id", class_name: "ListItem"
-  belongs_to :child , foreign_key: "child_id", class_name: "ListItem"
+  # belongs_to :parent , foreign_key: "parent_id", class_name: "ListItem"
+  # belongs_to :child , foreign_key: "child_id", class_name: "ListItem"
 
   # scopes
 
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -4,13 +4,13 @@ class ListItem < ApplicationRecord
   has_many :users, :through => :list_users
 
   # reference: https://medium.com/@jbmilgrom/active-record-many-to-many-self-join-table-e0992c27c1e
-  has_many :parents, through: :child_of_parents , source: :parent
-  has_many :child_of_parents, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
-  # has_many :parents, through: :parent_list_hierarchy_relationships , source: :parent
-  # has_many :parent_list_hierarchy_relationships, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
-
-  has_many :children, through: :parent_of_children, source: :child
-  has_many :parent_of_children, foreign_key: :parent_id, class_name: "ListHierarchyRelationship"
+  # has_many :parents, through: :child_of_parents , source: :parent
+  # has_many :child_of_parents, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
+  # # has_many :parents, through: :parent_list_hierarchy_relationships , source: :parent
+  # # has_many :parent_list_hierarchy_relationships, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
+  #
+  # has_many :children, through: :parent_of_children, source: :child
+  # has_many :parent_of_children, foreign_key: :parent_id, class_name: "ListHierarchyRelationship"
 
   # attachments
 
