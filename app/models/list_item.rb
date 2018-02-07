ba1cb3b3d547f58be8b183a35471ecda5d07de9f
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-06 21:43:12 -0600

    start documenting the SQL from different attempts

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -4,29 +4,43 @@ class ListItem < ApplicationRecord
   has_many :users, :through => :list_users
 
   # reference: https://medium.com/@jbmilgrom/active-record-many-to-many-self-join-table-e0992c27c1e
-  # has_many :parents, through: :child_of_parents , source: :parent
-  # has_many :child_of_parents, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
-  # has_many :parents, through: :parent_list_hierarchy_relationships , source: :parent
-  # has_many :parent_list_hierarchy_relationships, foreign_key: :child_id, class_name: "ListHierarchyRelationship"
-  # has_many :parents, :through => :child_of_parents , :source => :parent
-  # has_many :child_of_parents, :foreign_key => :child_id, :class_name => "ListHierarchyRelationship"
-  has_many :list_hierarchy_relationships, :foreign_key => :child_id, :class_name => "ListHierarchyRelationship"
+  # par, chi = ListItem.find(1,2)
+  # par.children  par.parents   chi.parents chi.children
+  #   CHI         null          PAR         null
+
+  # # this association works, but in the opposite direction
+  # has_many :list_hierarchy_relationships, :foreign_key => :child_id, :class_name => "ListHierarchyRelationship"
+  # has_many :parents, :through => :list_hierarchy_relationships , :source => :parent
+  #
+  # has_many :list_hierarchy_relationships, :foreign_key => :parent_id, :class_name => "ListHierarchyRelationship"
+  # has_many :children, :through =>  :list_hierarchy_relationships, :source => :child
+
+  # # par.children  par.parents   chi.parents chi.children
+  # #   null        null          PARENT      self
+  # par.children =>  null   ON `list_items`.`id` = `LHR`.`parent_id` WHERE `LHR`.`child_id` = 1
+  # par.parents  =>  self   ON `list_items`.`id` = `LHR`.`child_id` WHERE `LHR`.`child_id` = 1
+  # chi.parents  =>  null   ON `list_items`.`id` = `LHR`.`parent_id` WHERE `LHR`.`child_id` = 2
+  # chi.children =>  self   ON `list_items`.`id` = `LHR`.`child_id` WHERE `LHR`.`child_id` = 2
+  # has_many :list_hierarchy_relationships, :foreign_key => :parent_id, :class_name => "ListHierarchyRelationship"
+  # has_many :parents, :through => :list_hierarchy_relationships , :source => :parent
+  #
+  # has_many :list_hierarchy_relationships, :foreign_key => :child_id, :class_name => "ListHierarchyRelationship"
+  # has_many :children, :through =>  :list_hierarchy_relationships, :source => :child
+
+
+  # par.children =>  null   ON `list_items`.`id` = `LHR`.`child_id` WHERE `LHR`.`child_id` = 1
+  # par.parents  =>  self   ON `list_items`.`id` = `LHR`.`parent_id` WHERE `LHR`.`parent_id` = 1
+  # chi.parents  =>  null   ON `list_items`.`id` = `LHR`.`parent_id` WHERE `LHR`.`parent_id` = 2
+  # chi.children =>  self   ON `list_items`.`id` = `LHR`.`child_id` WHERE `LHR`.`child_id` = 2
+  has_many :list_hierarchy_relationships, :foreign_key => :parent_id, :class_name => "ListHierarchyRelationship"
   has_many :parents, :through => :list_hierarchy_relationships , :source => :parent
 
-
-  # has_many :children, through: :parent_of_children, source: :child
-  # has_many :parent_of_children, foreign_key: :parent_id, class_name: "ListHierarchyRelationship"
-  # has_many :children, :through =>  :parent_of_children, :source => :child
-  # has_many :parent_of_children, :foreign_key => :parent_id, :class_name => "ListHierarchyRelationship"
-  has_many :list_hierarchy_relationships, :foreign_key => :parent_id, :class_name => "ListHierarchyRelationship"
-  has_many :children, :through =>  :list_hierarchy_relationships, :source => :child
-
+  has_many :parental_relationships, :foreign_key => :child_id, :class_name => "ListHierarchyRelationship"
+  has_many :children, :through =>  :parental_relationships, :source => :child
 
   # attachments
 
 
-  # has_many :list_hierarchy_relationships, foreign_key: :parent_id
-  # has_many :list_hierarchy_relationships, foreign_key: :child_id
 
 
 
