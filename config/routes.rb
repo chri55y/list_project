861bab2c3ce1b8b453e9ad583f5cd28545cf0065
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-09 14:53:46 -0600

    list_items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'list_items/index,'
-
-  get 'list_items/show,'
-
-  get 'list_items/new,'
-
-  get 'list_items/edit,'
-
-  get 'list_items/delete'
-
   resources :users do
+    member do
+      get :delete
+    end
+  end
+
+  resources :list_items do
     member do
       get :delete
     end
