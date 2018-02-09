e7deb53ea89085f1dc6cbfdd8ba48019340f68fc
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-09 14:31:06 -0600

    add non-default actions to Users controller (#create, #update, #destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,21 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+
+  end
+
   def edit
   end
 
+  def update
+
+  end
+
   def delete
+  end
+
+  def destroy
+
   end
 end
