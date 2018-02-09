c6beb14592da8c37339d2ce0071caeb673df0396
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-09 13:01:19 -0600

    failed attempt

--- app/models/list_hierarchy_relationship.rb
+++ app/models/list_hierarchy_relationship.rb
@@ -2,7 +2,7 @@ class ListHierarchyRelationship < ApplicationRecord
   # associations
   # belongs_to :parent , foreign_key: "parent_id", class_name: "ListItem"
   # belongs_to :child , foreign_key: "child_id", class_name: "ListItem"
-  belongs_to :parent , :foreign_key => :parent_id, :class_name => "ListItem" #, :inverse_of => :parents
+  belongs_to :parent , :foreign_key => :parent_id, :class_name => "ListItem" , :inverse_of => :children
   belongs_to :child , :foreign_key => :child_id, :class_name => "ListItem"   #, :inverse_of => :children
 
   # scopes
