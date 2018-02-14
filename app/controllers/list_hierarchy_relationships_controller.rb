95cc27085e6ea4604227cf56f0296068af2cb867
chri55y <11790059+chri55y@users.noreply.github.com>
2018-02-14 13:25:26 -0600

    add non default CRUD actions to LHR controller

--- app/controllers/list_hierarchy_relationships_controller.rb
+++ app/controllers/list_hierarchy_relationships_controller.rb
@@ -8,9 +8,21 @@ class ListHierarchyRelationshipsController < ApplicationController
   def new
   end
 
+  def create
+
+  end
+
   def edit
   end
 
+  def update
+
+  end
+
   def delete
+  end
+
+  def destroy
+
   end
 end
